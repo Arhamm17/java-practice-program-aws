3603515cd246047c7460a81418aede7cd4aa021e
Muhammad Arham <muhammadarhamrashid12@gmail.com>
2026-04-05 17:22:40 +0500

    script.js code updated

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simulating frontend receiving data
+function displayMessage(message) {
+    console.log("Message from Java: " + message);
+}
+
+// Example usage
+displayMessage("Hello Class Calling -> Hello from MessageService!");
